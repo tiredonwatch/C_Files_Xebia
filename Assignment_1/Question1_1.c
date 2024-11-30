8b73af184c0dfd17a3cf6b85c5cab24dd7e0abec
Deepesh Yadav <yadavdeepesh2005@gmail.com>
2024-11-30 14:43:06 +0530

    Update Question1_1.c

--- Assignment_1/Question1_1.c
+++ Assignment_1/Question1_1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
+int main(void) {
     int number, originalNumber, remainder;
     int counter =0;
     int sum= 0;
@@ -36,5 +36,4 @@ int main() {
         printf("%d is not an Armstrong number.\n", number);
     }
 
-    return 0;
 }
